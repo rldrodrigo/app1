9b6950ca279265233e6a59b43fe9543f53ab6154
Rodrigo Lima <rodrigo.lima1945@gmail.com>
2021-10-18 19:50:00 -0300

    Inclusão do template do topo

--- src/app/topo/topo.component.ts
+++ src/app/topo/topo.component.ts
@@ -2,7 +2,8 @@ import { Component } from "@angular/core";
 
 @Component({
     selector: 'app-topo', //É onde define a "tag" para adicionar no arquivo
-    templateUrl: './topo.component.html' //Local do arquivo 
+    templateUrl: './topo.component.html', //Local do arquivo 
+    styleUrls: ['./topo.component.css']
 
 })
 
