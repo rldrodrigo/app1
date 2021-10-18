26245a7be15877c896459407bd68d56520b75b66
Rodrigo Lima <rodrigo.lima1945@gmail.com>
2021-10-18 16:26:18 -0300

    Criação do topo component
    Criação manual de um componente

---
+++ src/app/topo/topo.component.ts
@@ -0,0 +1,11 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: 'app-topo', //É onde define a "tag" para adicionar no arquivo
+    templateUrl: './topo.component.html' //Local do arquivo 
+
+})
+
+export class TopoComponent {
+
+}
